f9f3fc89b2ae6fb33d4ccdf8476885b1823e0e55
Sachin584 <ssolanki5841@gmail.com>
2020-01-06 21:34:50 +0530

    products add

--- shop/views.py
+++ shop/views.py
@@ -1,33 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Product
 from math import ceil
 
+# Create your views here.
+from django.http import HttpResponse
 
 def index(request):
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nSlides, 'range': range(1,nSlides), 'product': products}
-    return render(request, 'shop/index.html', params)
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
 
+    allProds = []
+    catprods = Product.objects.values('categary', 'id')
+    cats = {item['categary'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(categary=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, 'shop/search.html')
 
 def productView(request):
-    return HttpResponse("We are at product view")
+    return render(request, 'shop/prodView.html')
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
-
+    return render(request, 'shop/checkout.html')
